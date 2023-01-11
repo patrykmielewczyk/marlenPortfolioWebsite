9f31790bb69ba4a5b52a4b9f1be72c09f5e58335
patrykmielewczyk <72134743+patrykmielewczyk@users.noreply.github.com>
2023-01-11 12:23:52 +0100

    adding simple nav without styling, scale animated photo on the first section

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -2,22 +2,45 @@ export default function Nav() {
   return (
     <nav
       style={{
-        zIndex: "100",
-        backgroundColor: "white",
-        width: "50px",
-        height: "50px",
+        position: "fixed",
+        display: "flex",
+        flexWrap: "wrap",
+        zIndex: "5",
+        backgroundColor: "rgb(44,43,43,60%)",
+        width: "100%",
+        height: "80px",
       }}
     >
-      <a href="" style={{ color: "black" }}>
+      <a
+        href=""
+        style={{
+          color: "white",
+          marginRight: "10%",
+          marginLeft: "40%",
+          marginTop: "2%",
+        }}
+      >
         Home
       </a>
-      <a href="" style={{ color: "black" }}>
+      <a
+        href=""
+        style={{
+          color: "white",
+          marginTop: "2%",
+          marginRight: "10%",
+        }}
+      >
         Portfolio
       </a>
-      <a href="" style={{ color: "black" }}>
-        Contact
-      </a>
-      <a href="" style={{ color: "black" }}>
+
+      <a
+        href=""
+        style={{
+          color: "white",
+          marginTop: "2%",
+          marginRight: "10%",
+        }}
+      >
         About
       </a>
     </nav>
